5ec1698223bbdb979703e52e5aeb8d77b0db15f8
Joanne Lee <vernapii@gmail.com>
2018-03-08 20:06:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ function shout(string) {
 return string.toUpperCase ()
 }
 
+function whisper(string) {
+  return string.toLowerCase ()
+}
+function logWhisper(string) {
+  return console.log.string.toLowerCase 
+}
+
+}
